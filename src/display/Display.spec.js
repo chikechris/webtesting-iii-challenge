d3313fa49ef8043c4d8f1fa4023dcf0eff579c6d
chikechris <cchike2017@gmail.com>
2019-08-19 16:47:40 -0400

    display component test rendeers test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Display from './Display';
+import {
+  render,
+  fireEvent
+} from '@testing-library/react';
+
+describe('<Display /> Components tests', () => {
+  it('renders without crashing', () => {
+    render( < Display / > )
+  })
+})
