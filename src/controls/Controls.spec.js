16ff9289bd79481bc3b037f3680dfc96a43eae55
chikechris <cchike2017@gmail.com>
2019-08-20 04:22:47 -0400

    added controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,46 @@
-// Test away!
+import React from "react";
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+import Dashboard from "../dashboard/Dashboard";
+
+afterEach(cleanup);
+
+describe("<Controls />", () => {
+  it("Matches snapshot", () => {
+    const { container } = render(<Controls />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("Renders expected initial state", () => {
+    const { getByTestId } = render(<Dashboard />);
+    const lock = getByTestId("lock-btn");
+    const handle = getByTestId("handle-btn");
+
+    expect(lock).toHaveTextContent("Lock Gate");
+    expect(lock).toBeDisabled();
+    expect(handle).toHaveTextContent("Close Gate");
+    expect(handle).not.toBeDisabled();
+  });
+
+  it("Lock and Close as expected", () => {
+    const { getByTestId } = render(<Dashboard />);
+    const lock = getByTestId("lock-btn");
+    const handle = getByTestId("handle-btn");
+
+    //Close
+    fireEvent.click(handle);
+    expect(lock).toHaveTextContent("Lock Gate");
+    expect(lock).not.toBeDisabled();
+    expect(handle).toHaveTextContent("Open Gate");
+    expect(handle).not.toBeDisabled();
+
+    //Lock
+    fireEvent.click(lock);
+    expect(lock).toHaveTextContent("Unlock Gate");
+    expect(lock).not.toBeDisabled();
+    expect(handle).toHaveTextContent("Open Gate");
+    expect(handle).toBeDisabled();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,96 +1,96 @@
 // Test away!
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Display from './Display';
-// import {
-//   render
-// } from '@testing-library/react';
+import React from 'react';
 
-// describe('<Display /> Components tests', () => {
-//       it('renders without crashing', () => {
-//         render( < Display / > )
-//       })
+import Display from './Display';
+import {
+  render
+} from '@testing-library/react';
 
-//       it('renders closed false and locked false by default', () => {
-//         const {
-//           queryByText
-//         } = render( < Display / > );
-//         expect(queryByText(/open/i)).toBeTruthy();
-//         expect(queryByText(/unlocked/i)).toBeTruthy();
-//       })
+describe('<Display /> Components tests', () => {
+      it('renders without crashing', () => {
+        render( < Display / > )
+      })
 
-//       it('displays closed if the closed prop is true', () => {
-//             const {
-//               queryByText
-//             } = render( < Display closed = {
-//                 true
-//               }
-//               locked = {
-//                 true
-//               }
-//               />)
-//               expect(queryByText(/closed/i))
-//             })
+      it('renders closed false and locked false by default', () => {
+        const {
+          queryByText
+        } = render( < Display / > );
+        expect(queryByText(/open/i)).toBeTruthy();
+        expect(queryByText(/unlocked/i)).toBeTruthy();
+      })
 
-//           it(' displays lock if the lock prop is true', () => {
-//               const {
-//                 getByTest,
-//                 queryByText
-//               } = render( < Display closed = {
-//                   true
-//                 }
-//                 locked = {
-//                   true
-//                 }
-//                 />) 
-//                 expect(queryByText(/locked/i))
-//               })
+      it('displays closed if the closed prop is true', () => {
+            const {
+              queryByText
+            } = render( < Display closed = {
+                true
+              }
+              locked = {
+                true
+              }
+              />)
+              expect(queryByText(/closed/i))
+            })
 
-//             it(' displays open if the closed prop is false', () => {
-//                 const {
-//                   queryByText
-//                 } = render( < Display closed = {
-//                     false
-//                   }
-//                   locked = {
-//                     true
-//                   }
-//                   />)
-//                   expect(queryByText(/open/i))
-//                 })
+          it(' displays lock if the lock prop is true', () => {
+              const {
+                getByTest,
+                queryByText
+              } = render( < Display closed = {
+                  true
+                }
+                locked = {
+                  true
+                }
+                />) 
+                expect(queryByText(/locked/i))
+              })
 
-//               it('displays unlocked if the locked prop is false', () => {
-//                   const {
-//                     queryByText
-//                   } = render( < Display closed = {
-//                       false
-//                     }
-//                     locked = {
-//                       false
-//                     }
-//                     />) 
-//                     expect(queryByText(/unlocked/i))
-//                   })
+            it(' displays open if the closed prop is false', () => {
+                const {
+                  queryByText
+                } = render( < Display closed = {
+                    false
+                  }
+                  locked = {
+                    true
+                  }
+                  />)
+                  expect(queryByText(/open/i))
+                })
 
-//                 it('uses green-led class if locked or closed is false', () => {
-//                     const wrapper = render( < Display closed = {
-//                         false
-//                       }
-//                       locked = {
-//                         false
-//                       }
-//                       />)
-//                       const buttonList = wrapper.getAllByTestId('led green-led'); expect(buttonList.length).toBe(2);
-//                     })
+              it('displays unlocked if the locked prop is false', () => {
+                  const {
+                    queryByText
+                  } = render( < Display closed = {
+                      false
+                    }
+                    locked = {
+                      false
+                    }
+                    />) 
+                    expect(queryByText(/unlocked/i))
+                  })
 
-//                   it('uses red-led class if locked or closed is true', () => {
-//                       const wrapper = render( < Display closed = {
-//                           true
-//                         }
-//                         locked = {
-//                           true
-//                         }
-//                         />)
-//                         const buttonList = wrapper.getAllByTestId('led red-led'); expect(buttonList.length).toBe(2);
-//                       })
-//                   })
+               it('uses green-led class if locked or closed is false', () => {
+                    const wrapper = render( < Display closed = {
+                        false
+                      }
+                      locked = {
+                        false
+                      }
+                      />)
+                      const buttonList = wrapper.getAllByTestId('led green-led'); expect(buttonList.length).toBe(2);
+                    })
+
+              it('uses red-led class if locked or closed is true', () => {
+                      const wrapper = render( < Display closed = {
+                          true
+                        }
+                        locked = {
+                          true
+                        }
+                        />)
+                        const buttonList = wrapper.getAllByTestId('led red-led'); expect(buttonList.length).toBe(2);
+                      })
+                  })
